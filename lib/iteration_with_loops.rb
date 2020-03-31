e6aeac8724d6dac291478e967e53f735f584a9b6
Colton Kaiser <kaiserc2@seattleu.edu>
2020-03-31 03:31:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ def join_nested_strings(src)
     inner_count = 0
     while inner_count < src[count].length do
       if src[count][inner_count].class == String
-        joined_src << src[count][inner_count]
+        joined_src << src[count][inner_count] + " "
       end
       inner_count += 1
     end
